8db1c23c8c8115ed7cf06e4c83dc5af676623c30
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-05-18 15:18:05 +0530

    update position

---
+++ seller_dashboard.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Click Cart</title>
+    <link rel="stylesheet" href="./css/sellerDashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Oswald:wght@200&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+    <header id="seller_header">
+        <nav class="seller_nav">
+	        <div class="logo">
+	            <a href="#">
+	                <img src="./images/logo.png" alt="place-holder"/>
+	            </a>
+	        </div>
+            <ul type="1">
+            	<li><a href="#">Seller Account</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Help Center</a></li>
+                <li><a href="#">My Business</a></li>
+            </ul>
+        </nav>
+    </header> 
+
+    <div style="padding: 5px; display: flex; margin-top: 10px;">
+    	<div class="sideBar">
+    		<div style="margin-bottom: 10px;">
+    			<div style="display:flex; align-items: center; margin-bottom: 10px;">
+	    			<div class="avatar_s">
+		    			<img src="./images/avatar.png" alt="avatar">
+		    		</div>
+		    		<div style="position: relative; left: 10%;">
+		    			<h3>Seler Name</h3>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star checked"></span>
+						<span class="fa fa-star"></span>
+						<span class="fa fa-star"></span>
+		    		</div>
+    			</div>
+
+	    		<hr/>
+	    		<div>
+	    			<p>Pending Order</p>
+	    			<p>Pending Order</p>
+	    			<p>Pending Order</p>
+	    			<hr/>
+	    			<p>Earn in Month</p>
+	    		</div>
+	    	</div>
+	    	<div class="chats">
+	    		<table border="1px">
+	    			<tr>
+	    				<th style="display:flex;">
+	    					<p>Heading</p>
+	    					<a href="#">View All</a>
+	    				</th>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    		</table>
+	    	</div>
+    	</div>
+
+    	<div class="mainContent">
+    		<button placeholder="Add New">Add New</button>
+    		<div style="display:flex; flex-wrap: wrap; margin-top: 10px;">
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%; cursor: pointer;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    					
+
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    		
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    					
+    			    	 	
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    		  			
+    			    	   	
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    		 			
+    			    	   	
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    		  			
+    			    	  	
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    					
+    			    	   	
+    			<div class="sProduct">
+    				<img src="./images/watch.png">
+    				<div style="display:flex; overflow: hidden; width: 100%;">
+    					<p>$15.00</p>
+    					<i style="position: flex; right: 5px; margin-left: 130px;"  class="fa-solid fa-bars"></i>
+    				</div>
+    			</div>  			
+    			    		 			
+    			    	    			
+    		</div>
+    	</div>
+    </div>
+
+    <div class="bottomBar">
+   		<div class="bottomCont">
+	    		<table border="1px">
+	    			<tr>
+	    				<th style="display:flex;">
+	    					<p>Heading</p>
+	    					<a href="#">View All</a>
+	    				</th>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    		</table>
+   		</div>
+   		<div class="bottomCont">
+	    		<table border="1px">
+	    			<tr>
+	    				<th style="display:flex;">
+	    					<p>Heading</p>
+	    					<a href="#">View All</a>
+	    				</th>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    		</table>
+   		</div>
+
+   		<div class="bottomCont">
+	    		<table border="1px">
+	    			<tr>
+	    				<th style="display:flex;">
+	    					<p>Heading</p>
+	    					<a href="#">View All</a>
+	    				</th>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    			<tr>
+	    				<td>
+	    					<p>Name</p>
+	    					<span>Lorem ipsume is dummy text</span>
+	    				</td>
+	    			</tr>
+	    		</table>
+   		</div>
+    </div>
+    <br/>
+
+    <?php include './footer.php' ?>
+
+</body>
+</html>
