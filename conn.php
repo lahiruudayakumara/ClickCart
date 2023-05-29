2ae25ded759de8fc57768a1dd2d301d12908d5f6
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-05-29 13:29:13 +0530

    UPDATE CONN

--- conn.php
+++ conn.php
@@ -7,10 +7,10 @@
 
 // Create connection
 
-    $con = new mysqli($servername, $username, $password, $db);
+    $con = mysqli_connect($servername, $username, $password, $db);
 // Check connection
 
-    if ($con->connect_error) {
+    if (mysqli_connect_error()) {
         die("Connection failed: " . $con->connect_error);
     }
     echo "<script>console.log('Database Connect Successful')</script>";
