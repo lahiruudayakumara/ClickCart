0c4d84942693b7db4fdbc5a6b935137eceac7222
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-05-30 05:07:33 +0530

    complete product delete item

---
+++ product_delete.php
@@ -0,0 +1,27 @@
+<?php
+
+    session_start();
+
+    include './conn.php';
+
+    if ($_SESSION['user_role'] == 'seller') { 
+
+
+            $id = $_GET['id'];
+
+            $query2 = "DELETE FROM product WHERE product_ID = $id  LIMIT 1";
+
+            $result_set = mysqli_query($con, $query2);
+
+            if ($result_set) {
+                echo  '<script>alert("Your Product Item Deleted"); window.location = "seller_dashboard.php"; </script>';
+            } else {
+               echo  '<script>alert("News Delete Fail"); window.location = "seller_dashboard.php"; </script>';
+            }
+    } else {
+        header('Location : login.php');
+        exit();
+    }
+
+    mysqli_close($con);
+?>
