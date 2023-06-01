91af5d9937bb8898a3c5b1c7f3589d5c08238cbe
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-06-02 05:27:23 +0530

    Update seller_dashboard.php

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -2,7 +2,7 @@
 
 	session_start();
 
-	include './conn.php';
+	require './conn.php';
 
 
 	if ($_SESSION['user_role'] == "seller" ) { 
@@ -30,79 +30,78 @@
 		                <img src="./images/logo.png" alt="place-holder"/>
 		            </a>
 		        </div>
-
 	            <ul>
-	            	<li><a href="./logout.php">Log Out</a></li>
-	                <li><a href="#">Contact Us</a></li>
-	                <li><a href="./helpcenter.php">Help Center</a></li>
-	                <li><a href="#">My Business</a></li>
-
+					<li class="li"><a href="#">My Business</a></li>
+					<li class="li"><a href="./helpcenter.php">Help Center</a></li>
+					<li class="li"><a href="#">Contact Us</a></li>
+	            	<li class="li-dropdown">
+						Seller
+						<ul id="child-ul" class="child-ul" > 
+							<li ><a href="#">Manage Account</a></li>
+							<li><a href="./logout.php">Logout</a></li>
+						</ul>
+					</li>
 	            </ul>
-
+				<label>
+    				<i class="fa fa-bars"></i>
+  				</label>
 	        </nav>
 	    </header> 
-
 	    <?php
 	    $category = "SELECT * FORM category";
 	    $cat = mysqli_query($con, $category);
 	    ?>
 
-	  <div id="add-product" class="add-product">
-	    <h2>Add Product</h2>
-	    <form method="POST" action="product_add.php">
-					<select>
-						<?php 
-							$selectCategory = "SELECT * FROM category";
-							$ca = mysqli_query($con, $selectCategory);
+		<div id="add-product" class="add-product">
+	    	<h2>Add Product</h2>
+			<form method="POST" action="product_add.php" enctype="multipart/form-data">
+				<select name="category" id="category">
+					<?php 
+						$query1 = "SELECT * FROM category";
+						$result1 = $con->query($query1);
 
-							if($ca) {
-								while ($rowCategory = mysqli_fetch_assoc($ca)) {
-									$c_ID =$rowCategory['category_ID'];
-									$c_Name =$rowCategory['category_Name'];
+						if($result1) {
+							while ($row = $result1->fetch_assoc()) {
 
-									?>
-										<option value="<?php echo $c_ID; ?>"><?php echo $c_Name; ?></option>
-									<?php
-								}
+								echo '<option value="'. $row['category_ID'] . '">' . $row['category_Name'] . '</option>';
+
 							}
-						?>
-					</select>
+						}
+					?>
+				</select>
 					<input type="text" name="name"><br/>
 					<input type="text" name="price"><br/>
 					<input type="file" name="image"><br/>
-	      <input class="submit"  type="submit" value="Submit">
-	      <button class="cancel" type="button"  onclick="hidePopup()">Cancel</button>
-	    </form>
-	  </div>
+				<input class="submit" name="submit"  type="submit" value="Submit">
+				<button class="cancel" type="button"  onclick="hidePopup()">Cancel</button>
+			</form>
+		</div>
 
 
-	  <div id="edit-product" class="edit-product">
-	    <h2>Add Product</h2>
-	    <form method="POST" action="product_add.php">
-					<select>
-						<?php 
-							$selectCategory = "SELECT * FROM category";
-							$ca = mysqli_query($con, $selectCategory);
+		<div id="edit-product" class="edit-product">
+	    	<h2>Add Product</h2>
+	    	<form method="POST" action="product_add.php">
+				<select name="category" id="category">
+					<?php 
+						$query = "SELECT * FROM category";
+						$result = $con->query($query);
 
-							if($ca) {
-								while ($rowCategory = mysqli_fetch_assoc($ca)) {
-									$c_ID =$rowCategory['category_ID'];
-									$c_Name =$rowCategory['category_Name'];
+						if($result1) {
+							while ($row = $result1->fetch_assoc()) {
 
-									?>
-										<option value="<?php echo $c_ID; ?>"><?php echo $c_Name; ?></option>
-									<?php
-								}
+								echo '<option value="'. $row['category_ID'] . '">' . $row['category_Name'] . '</option>';
+
 							}
-						?>
-					</select>
-					<input type="text" name="name"><br/>
-					<input type="text" name="price"><br/>
-					<input type="file" name="image"><br/>
-	      <input class="submit"  type="submit" value="Submit">
-	      <button class="cancel" type="button"  onclick="hidePopup()">Cancel</button>
-	    </form>
-	  </div>
+						}
+					?>
+				</select>
+				<input type="text" name="name"><br/>
+				<input type="text" name="price"><br/>
+				<input type="file" name="image"><br/>
+	    		<input class="submit"  type="submit" value="Submit">
+	    		<button class="cancel" type="button"  onclick="hidePopup()">Cancel</button>
+	    	</form>
+	  	</div>
 
 
 	    <div class="main_section">
@@ -116,31 +115,26 @@
 					    <?php 
 					        $sellerID = $_SESSION["seller_ID"];
 
-							$query ="SELECT * FROM seller where seller_ID =  $sellerID ";
+							$query3 ="SELECT * FROM seller where seller_ID =  $sellerID ";
+							$result3 = $con->query($query3);
 
-							$result = mysqli_query($con, $query);
-
-							if ($result) {
+							if ($result3) {
 								
-								$row = mysqli_fetch_assoc($result) ;
+								$row = $result3->fetch_assoc();
 								$sID = $row['seller_ID'];
 								$sName =  $row['seller_Name'];
-								
+								echo "<h3>" . $sName . "</h3>";
 							}
-			                ?>
-					    	<h3><?php echo "$sName";?></h3>
-					    	<?php
 
-					    		$stars = array(1, 2, 3, 4, 5);
-					    		$count = array();
-					    		$total = 0;
+					    	$stars = array(1, 2, 3, 4, 5);
+					    	$count = array();
+					    	$total = 0;
 					    		
+					    	foreach ($stars as $star) {
+					    			$query4 = "SELECT * FROM rating WHERE seller_ID = $sID AND stars = $star";
+					    			$result4 = $con->query($query4);
 
-					    		foreach ($stars as $star) {
-					    			$query = "SELECT * FROM rating WHERE seller_ID = $sID AND stars = $star";
-					    			$result = mysqli_query($con, $query);
-
-					    			$count[$star] = mysqli_num_rows($result);
+					    			$count[$star] = $result4->num_rows;
 					    			$total += 	$count[$star];			    			
 					    		}
 								
@@ -151,11 +145,7 @@
 								} else {
 									$rate = $calstar / $total;
 								}
-
-
-
-
-					    	?>
+					    ?>
 							<span id="stars" class="<?php if($rate >= 1) {echo "fa fa-star checked";} else if (0 < $rate and $rate < 1) {echo "fa fa-star-half-alt";} else {echo "far fa-star";} ?>"></span>
 							<span id="stars" class="<?php if($rate >= 2) {echo "fa fa-star checked";} else if (1 < $rate and $rate < 2) {echo "fa fa-star-half-alt";} else {echo "far fa-star";} ?>"></span>
 							<span id="stars" class="<?php if($rate >= 3) {echo "fa fa-star checked";} else if (2 < $rate and $rate < 3) {echo "fa fa-star-half-alt";} ?>"></span>
@@ -166,123 +156,75 @@
 	    			</div>
 
 		    		<hr/>
+
 		    		<?php
 		    			$types = array("pending", "complete", "cancel");
-		    			$o_counts = array();
+		    			$order_Counts = array();
 
 		    			foreach ($types as $type){
 
-			    			$query_p_o_count = "SELECT * FROM order_items WHERE seller_ID = $sellerID AND  order_Type = '$type'";
-			    			$query_p_o_count_result = mysqli_query($con, $query_p_o_count);
-			    			$o_counts[] = mysqli_num_rows($query_p_o_count_result);
+			    			$query5 = "SELECT * FROM order_items WHERE seller_ID = $sellerID AND  order_Type = '$type'";
+			    			$result5 = $con->query($query5);
+			    			$order_Counts[] = $result5->num_rows;
 
 		    			}
-
 
 		    			$query_p_o_count = "SELECT * FROM order_items WHERE seller_ID = $sellerID AND  order_Type = 'pending' ";
 		    			$query_p_o_count_result = mysqli_query($con, $query_p_o_count);
 		    			$query_p_o_row = mysqli_num_rows($query_p_o_count_result);
-
-
 		    		?>
 
 		    		<div>
-		    			<p>Pending Order <span style="float: right;"><?php echo $o_counts[0]; ?></span></p>
-		    			<p>Complete Order <span style="float: right;"><?php echo $o_counts[1]; ?></span></p>
-		    			<p>Cancle Order <span style="float: right;"><?php echo $o_counts[2]; ?></span></p>
+		    			<p>Pending Order <span style="float: right;"><?php echo $order_Counts[0]; ?></span></p>
+		    			<p>Complete Order <span style="float: right;"><?php echo $order_Counts[1]; ?></span></p>
+		    			<p>Cancle Order <span style="float: right;"><?php echo $order_Counts[2]; ?></span></p>
 		    			<hr/>
 		    			<p>Earn in Month <span style="float: right;">$<?php echo"need" ?></span></p>
 		    		</div>
 		    	</div>
 		    	<hr>
 
-		    	<div class="chat">
-		    	<table>
-			    	<tr>
-			    		<th><p class="inline" style="float:left;">Chat</p><p class="a_inline_right">View All</p></th>
-			    	</tr>
-
-
-
-
-
-
-			    	<?php
-			    		$messages_query = "SELECT * FROM messages WHERE sender = 'seller' AND seller_ID = $sellerID AND buyer_ID = 1 LIMIT 2";
-
-			    		$messages_result = mysqli_query($con, $messages_query);
-
-			    		if($messages_result ) {
-
-			    			while($row = mysqli_fetch_assoc($messages_result)) {
-			    				$message = $row['message'];
-			    				$buyer = $row['buyer_ID'];
-			    				$timestamp = $row['timestamp']
-			    				?>
-				    			<tr>
-				    				<td>
-				    					<img style="
-				    					position: absolute;" src="./images/avatar.jpg" alt="avatar" width="40px" height="40px">
-				    					<div style="margin-left: 50px;"> 
-				    												    			<p class="inline"><?php echo $buyer . $message; ?></p><br/>
-						    			<p class="inline"><?php echo $buyer . $message; ?></p>
-						    			<span style="font-size:12px; margin-right: 5px;"  class="p_right" align="center"><?php echo $timestamp ?></span>
-						    		</div>
-					    			</td>
-					    		</tr>
-
-			    				<?php
-			    			}
-			    		}
-			    	?>
-			    </table>
-			    </div>
 	    	</div> 
 
 	    	<div class="mainContent">
-	    		<button placeholder="Add New" onclick="showPopup()">Add New</button>
+				<div class="btn-area">
+    				<button placeholder="Add New" onclick="showPopup()">Add New</button>
+				</div>
 
 	    		<div class="mainProduct">
-		    			<?php 
+		    		<?php 
 
-							$query2 ="SELECT product.*, category.*
-									  FROM product
-									  JOIN category ON product.category_ID = category.category_ID
-									  WHERE product.seller_ID = $sellerID
-										";
+						$query6 =  "SELECT product.*, category.*
+									FROM product
+									JOIN category ON product.category_ID = category.category_ID
+								  	WHERE product.seller_ID = $sellerID
+									";
 
-							$result2 = mysqli_query($con, $query2);
+						$result6 = $con->query($query6);
 
-							if ($result2) {
-								
-								while ($row2 = mysqli_fetch_assoc($result2)) {
-									$pID = $row2['product_ID'];
-									$img =  $row2['product_Image'];
-								 	$price =  $row2['product_Price'];
-								 	$name =  $row2['product_Name'];
-								 	$ca =  $row2['category_Name'];
-						?>
+						if ($result6) {
+							
+							while ($row = $result6->fetch_assoc()) {
 
-			    			<div class="sProduct">
-			    				<img src="./images/<?php echo "$img"; ?>" alt="product_image">
-			    				<p><?php echo $ca . "<br/>" . $name; ?></p>
-			    				<div class="sProductCont">
-			    					<p>$<?php echo $price; ?></p>
-			    					<div class="clickIcon">
-			    						<i id="toggoleicon<?php echo $pID; ?>"  class="fa-solid fa-bars" onclick="clickbtn(<?php echo $pID; ?>)"></i>
-				    					<div id="clickBtn<?php echo $pID; ?>" class="clickBtn">
-				    						<a href="product_edit.php?id=<?php echo "$pID"; ?>">Edit Product</a><br/>
-				    						<a href="product_delete.php?id=<?php echo"$pID"; ?>">Delete Product</a>
-				    					</div>
+					?>
+		    			<div class="sProduct">
+		    				<img src="./images/<?php echo $row['product_Image']; ?>" alt="product_image">
+		    				<p><?php echo$row['category_Name'] . "<br/>" . $row['product_Name']; ?></p>
+		    				<div class="sProductCont">
+		    					<p>$<?php echo $row['product_Price']; ?></p>
+		    					<div class="clickIcon">
+		    						<i id="toggoleicon<?php echo $row['product_ID']; ?>"  class="fa-solid fa-bars" onclick="clickbtn(<?php echo $row['product_ID']; ?>)"></i>
+			    					<div id="clickBtn<?php echo $row['product_ID']; ?>" class="clickBtn">
+			    						<a href="product_edit.php?id=<?php echo $row['product_ID']; ?>">Edit Product</a><br/>
+			    						<a href="product_delete.php?id=<?php echo $row['product_ID']; ?>">Delete Product</a>
 			    					</div>
-			    				</div>
-			    			</div>	
+		    					</div>
+		    				</div>
+		    			</div>	
 						<?php
-								
 								}	
 							}
-	                ?>	 		  				 			
-	    			    	    			
+		                ?>   	    			
 	    		</div>
 	    	</div>
 	    </div>
@@ -298,23 +240,22 @@
 		    		<p class="inline">Product Name</p><p class="p_right">Buyer Name (Q.)</p>
 		    	</div>
 		    	<?php
-		    		$query_p_order = "  SELECT order_items.*, seller.*, buyer.*, product.*
-		    							FROM order_items 
-		    							JOIN seller ON order_items.seller_ID = seller.seller_ID
-		    							JOIN buyer ON order_items.buyer_ID = buyer.buyer_ID
-		    							JOIN product ON order_items.product_ID = product.product_ID
-		    							WHERE order_items.order_Type = 'pending' AND order_items.seller_ID = $sellerID
-		    							";
-		    		$result_p_order = mysqli_query($con, $query_p_order);
+		    		$query7 =  "SELECT order_items.*, seller.*, buyer.*, product.*
+		    					FROM order_items 
+		    					JOIN seller ON order_items.seller_ID = seller.seller_ID
+		    					JOIN buyer ON order_items.buyer_ID = buyer.buyer_ID
+		    					JOIN product ON order_items.product_ID = product.product_ID
+		    					WHERE order_items.order_Type = 'pending' AND order_items.seller_ID = $sellerID
+		    					";
 
-		    		if($result_p_order) {
+		    		$result7 = $con->query($query7);
 
-		    			while($row = mysqli_fetch_assoc($result_p_order)) {
-		    				$p_o_Name = $row['product_Name'];
-		    				$p_o_b_Name = $row['fName'];
-		    				$p_o_q_Quantity = $row['quantity'];
+		    		if($result7) {
+
+		    			while($row = $result7->fetch_assoc()) {
 		    				?>
-			    			<p class="inline"><?php echo $p_o_Name; ?></p><p class="p_right"><?php echo $p_o_b_Name . "&nbsp; &nbsp;" . $p_o_q_Quantity . "&nbsp &nbsp"; ?></p>
+			    			<p class="inline"><?php echo $row['product_Name']; ?></p>
+							<p class="p_right"><?php echo $row['fName'] . "&nbsp; &nbsp;" . $row['quantity'] . "&nbsp &nbsp"; ?></p>
 		    				<?php
 		    			}
 		    		}
@@ -330,23 +271,21 @@
 		    		<p class="inline">Product Name</p><p class="p_right">Buyer Name (Q.)</p>
 		    	</div>
 		    	<?php
-		    		$query_p_order = "  SELECT order_items.*, seller.*, buyer.*, product.*
+		    		$query8 = "  SELECT order_items.*, seller.*, buyer.*, product.*
 		    							FROM order_items 
 		    							JOIN seller ON order_items.seller_ID = seller.seller_ID
 		    							JOIN buyer ON order_items.buyer_ID = buyer.buyer_ID
 		    							JOIN product ON order_items.product_ID = product.product_ID
 		    							WHERE order_items.order_Type = 'complete' AND order_items.seller_ID = $sellerID
 		    							";
-		    		$result_p_order = mysqli_query($con, $query_p_order);
+		    		$result8 = $con->query($query8);
 
-		    		if($result_p_order) {
+		    		if($result8) {
 
-		    			while($row = mysqli_fetch_assoc($result_p_order)) {
-		    				$p_o_Name = $row['product_Name'];
-		    				$p_o_b_Name = $row['fName'];
-		    				$p_o_q_Quantity = $row['quantity'];
+		    			while($row = $result8->fetch_assoc()) {
 		    				?>
-			    			<p class="inline"><?php echo $p_o_Name; ?></p><p class="p_right"><?php echo $p_o_b_Name . "&nbsp; &nbsp;" . $p_o_q_Quantity . "&nbsp &nbsp"; ?></p><br/>
+			    			<p class="inline"><?php echo $row['product_Name']; ?></p>
+							<p class="p_right"><?php echo $row['fName'] . "&nbsp; &nbsp;" . $row['quantity'] . "&nbsp &nbsp"; ?></p><br/>
 		    				<?php
 		    			}
 		    		}
@@ -362,23 +301,21 @@
 		    		<p class="inline">Product Name</p><p class="p_right">Buyer Name (Q.)</p>
 		    	</div>
 		    	<?php
-		    		$query_p_order = "  SELECT order_items.*, seller.*, buyer.*, product.*
+		    		$query9 = "  SELECT order_items.*, seller.*, buyer.*, product.*
 		    							FROM order_items 
 		    							JOIN seller ON order_items.seller_ID = seller.seller_ID
 		    							JOIN buyer ON order_items.buyer_ID = buyer.buyer_ID
 		    							JOIN product ON order_items.product_ID = product.product_ID
 		    							WHERE order_items.order_Type = 'cancel' AND order_items.seller_ID = $sellerID
 		    							";
-		    		$result_p_order = mysqli_query($con, $query_p_order);
+		    		$result9 = $con->query($query9);
 
-		    		if($result_p_order) {
+		    		if($result9) {
 
-		    			while($row = mysqli_fetch_assoc($result_p_order)) {
-		    				$p_o_Name = $row['product_Name'];
-		    				$p_o_b_Name = $row['fName'];
-		    				$p_o_q_Quantity = $row['quantity'];
+		    			while($row = $result9->fetch_assoc()) {
 		    				?>
-			    			<p class="inline"><?php echo $p_o_Name; ?></p><p class="p_right"><?php echo $p_o_b_Name . "&nbsp; &nbsp;" . $p_o_q_Quantity . "&nbsp &nbsp"; ?></p>
+			    			<p class="inline"><?php echo $row['product_Name'];?></p>
+							<p class="p_right"><?php echo$row['fName'] . "&nbsp; &nbsp;" . $row['quantity'] . "&nbsp &nbsp"; ?></p>
 		    				<?php
 		    			}
 		    		}
@@ -388,8 +325,7 @@
 	    </div>
 	    <br/>
 
-	   
-
+		<?php include'./footer.php' ?>
 	    <!-- js files -->
 	    <script type="text/javascript" src="./js/check_online.js"></script>
 	    <script type="text/javascript" src="./js/sellerDashboard.js"></script>
@@ -400,4 +336,5 @@
 	} else {
 		header("Location: login.php");
 	}
+	$con->close();
 ?>
