f31c507074b9f8644c26738a502eb26aeefa83ab
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-06-02 17:51:50 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,59 @@
 <?php
 	require './conn.php';
 
-	if (isset($_POST['submit'])) {
+	session_start();
+
+	if(isset($_POST['submit']) && $_POST['email'] && $_POST['password']) {
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 
-		$query = "SELECT * FROM seller WHERE email= $email AND password= $password ";
-		$result = mysqli_query($con, $query);
-		$row_count = mysqli_num_rows($result);
+		$query = "SELECT * FROM seller WHERE email = '$email' AND password = '$password'";
+		$result = $con->query($query);
+		$rowCount = $result->num_rows;
 
-		if ($row_count == 1) {
-			// Fetch the row data
-			$row = mysqli_fetch_assoc($result);
+		$query2 = "SELECT * FROM buyer WHERE email = '$email' AND password = '$password'";
+		$result2 = $con->query($query2);
+		$rowCount2 = $result2->num_rows;
 
-			// Access specific column values
+		if($rowCount == 1) {
+			$row = $result->fetch_assoc();
+
 			$sellerID = $row['seller_ID'];
-			// ...
 
-			// Seller login successful
+			$_SESSION['user_role'] = 'seller';
+			$_SESSION['seller_ID'] = $sellerID;
+			
+			$sellerID = $row['seller_ID'];
+
+			echo "<script>alert('$rowCount')</script>";
+
 			$_SESSION['user_role'] = 'seller';
 			$_SESSION['seller_ID'] = $sellerID;
 			header("Location: seller_dashboard.php"); // Redirect to seller dashboard page
 			exit();
+			
+		} else if($rowCount2 == 1) {
 
+			$row = $result2->fetch_assoc();
 
+			$sellerID = $row['buyer_ID'];
+
+			$_SESSION['user_role'] = 'buyer';
+			$_SESSION['buyer_ID'] = $buyerID;
+			
+			$sellerID = $row['buyer_ID'];
+
+			$_SESSION['user_role'] = 'seller';
+			$_SESSION['seller_ID'] = $sellerID;
+			header("Location: index.php"); // Redirect to seller dashboard page
+			exit();
 		} else {
-			$error = "Invalid password";
+			$error = "invalid email password!";
 		}
-	} else {
-		echo "<script>alert('not work');</script>";
-	}
+	} 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,43 +61,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="./css/header.css">
 	<link rel="stylesheet" href="./css/style.css">
 	<link rel="stylesheet" href="./css/login.css">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Oswald:wght@200&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body> 
+<body>
 
     <!-- Header -->
 	<?php include './header.php'; ?>
 
     <!-- Login form -->
     <div class="login-box">
-      <h1>Hello</h1>
-		<p class="para-1">    <!-- link to create account page -->
-      Sign in to ClickCart or <a href="./create_account.php">create an account</a>
+      <h1>Login</h1>
+        <form action="login.php" method="post">
+			<p class="para-3"><?php if(isset($error)) { echo $error; } ?></p>
+            <input type="email" name="email" placeholder="Email" />
+            <input type="password" name="password" placeholder="password" />
+            <button name="submit">submit</button>
+      <closeform></closeform>
+	</form>
+	<p class="para-2">
+      Not have an account? <a href="signup.html">Sign Up Here</a>
     </p>
-
-		<?php if(isset($error)) { 
-			 ?> 
-			 <p class= "para-2"> Invalid password !! </p>
-			 <?php
-			} ?>
-        	<form action ="./login.php" method="POST">
-            	<input type="email" name="email" placeholder="Email" />
-            	<input type="password" name="password" placeholder="Password" />
-            <div class="recover"> 
-				<a href= '#'> Forgot password? </a>
-		    </div>
-			<input type="submit" name="submit">
-
-			<!--<button type="submit" name="submit">Login</button>
-      		<closeform></closeform>-->
-		</form>
     </div>
-    
+
 
     <!-- Footer -->
     <?php include './footer.php' ?>
