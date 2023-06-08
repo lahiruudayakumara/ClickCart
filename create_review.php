bc3fdcff94ffa828c9b48de726c1637ae3179cc6
IT22913838 <106859423+IT22913838@users.noreply.github.com>
2023-06-09 00:33:13 +0530

    update

--- create_review.php
+++ create_review.php
@@ -1,3 +1,4 @@
+<!-- add login verifycation here -->
 <?php
 	require './conn.php';
 
@@ -52,19 +53,58 @@
 		}
 	} 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $rating = $_POST["rating"];
-  $review = $_POST["review"];
-
-  $stmt = $conn->prepare("INSERT INTO reviews (name, email, rating, review) VALUES (?, ?, ?, ?)");
-  $stmt->bind_param("ssis", $name, $email, $rating, $review);
-  $stmt->execute();
-  $stmt->close();
-  $conn->close();
-
-  header("Location: review_page.php");
-  exit;
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/header.css">
+	<link rel="stylesheet" href="./css/review_page.css">
+	<link rel="stylesheet" href="./css/footer.css">
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Oswald:wght@200&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <!-- Header -->
+	<?php include './header.php'; ?>
+
+	<!-- Add review section Start -->
+<section class = "account-form">
+
+<form action = "" method ="POST">
+   	<h3>Post Your Review </h3>
+   	<p class ="placeholder"> review title <span>*</span> </p>
+   	<input type ="text" name="title" required maxlength="50" placehoder="enter review title" class="box">
+   	<p class="placeholder">review description</p>
+	<textarea name ="description" class="box" placeholder="enter description" maxlength="1000" cols="30" rows="10"></textarea>
+   	<p class="placeholder">review rating<span>*</span></p>
+   	<select name="rating"  class="box" required> 
+	   <option value="1">1</option>
+	   <option value="2">2</option>
+	   <option value="3">3</option>
+	   <option value="4">4</option>
+	   <option value="5">5</option>
+   	</select>
+	<input type="submit" value="submit review" name="submit" class="btn">
+	<a href ="store.php?get_Id=<?= $get_id; ?>"  clas="option-btn">go back</a>
+</form>
+
+</section>
+   <!-- Add review section ends -->
+    <!-- Footer -->
+    <?php include './footer.php' ?>
+
+</body>
+
+</html>
+<?php
+	$con->close();
+?>
+
