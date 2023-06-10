bec55a2aeef8100b8be96a0b5110beca97bdd91c
BinukiMihara <133636115+BinukiMihara@users.noreply.github.com>
2023-06-10 22:23:00 +0530

    buyer acc mng

--- buyer_account_manage_edit.php
+++ buyer_account_manage_edit.php
@@ -1,33 +1,46 @@
 <?php 
+
 	require './conn.php'; 
-	if(!isset($_SESSION['user_role'] == 'buyer')){
-		header('loction:login.php');
-	}
-    else{if(isset($_REQUEST['submit'])){
-		$bfname=$_POST['fname'];
-		$blname=$_POST['lname'];
-		$bbday=$_POST['bday'];
-		$baddress=$_POST['address'];
 
-		if((!empty($bfname))&&(!empty($blname))&&(!empty($bbday))&&(!empty($baddress))){
-			$bID=$_SESSION['buyer_ID'];
-			$query="UPDATE buyer SET fname='$bfname', lname='$blname', bday='$bbday', address='$baddress' WHERE buyer_ID='$bID'";
-			$con->query($query) {
-				echo "<script>alert('Profile upadeted sucessfully'); window.location = 'buyer_account_manage_edit.php'</script>";
-				exit;
-			} 
+	session_start();
+
+	if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'buyer') {
+		$bID = $_SESSION['buyer_ID'];
+
+		if(isset($_POST['update'])){
+			$bfname = $_POST['fname'];
+			$blname = $_POST['lname'];
+			$baddress = $_POST['address'];
+
+			$query="UPDATE buyer SET fName='$bfname', lName='$blname', address='$baddress' WHERE buyer_ID= '$bID'";
+		
+			if ($con->query($query)) {
+
+				echo "<script>alert('Profile upadeted sucessfully'); window.location = 'buyer_account_manage.php';</script>";
+			}  else {
+				echo "<script>alert('Profile upadeted Unsucessfully'); window.location = 'buyer_account_manage.php';</script>";
+			}	
+		} else if(isset($_POST['change'])) {
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+
+			$query1="UPDATE buyer SET email = '$email', password = '$password' WHERE buyer_ID= '$bID'";
+			if ($con->query($query1)) {
+
+				echo "<script>alert('Email & Password upadeted sucessfully'); window.location = 'buyer_account_manage.php';</script>";
+			}  else {
+				echo "<script>alert('Email & Password upadeted Unsucessfully'); window.location = 'buyer_account_manage.php';</script>";
+			}	
+		} else {
+			header('location: buyer_account_manage.php');
+			exit();
 		}
-
-		else {
-			$con->query($query) {
-			echo "<script>alert('First name,Last name,Birthday,Address are required );</script>; window.location = 'buyer_account_manage_edit.php'</script>";
-			exit;
-			}
-		}
+		
+	} else {
+		header('location: login.php');
+		exit();
 	}
-	}
-	
 
+	 $con->close(); 
 
 ?>
-<?php $con->close(); ?>
